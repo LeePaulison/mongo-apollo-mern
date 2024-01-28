853147b1ac51982a8d647d7148f0b9a7f66449b0
L0ngshanks <lee.paulison.jr@gmail.com>
2024-01-28 15:58:15 -0500

    Server conn.js and server.js added async and await

--- server/db/conn.js
+++ server/db/conn.js
@@ -8,15 +8,21 @@ const client = new MongoClient(Db, {
 var _db;
 
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db) {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB.");
-      }
-      return callback(err);
-    });
+  connectToServer: async function (callback) {
+    try {
+      await client.connect();
+    } catch (err) {
+      console.log(err);
+    }
+
+    _db = client.db("employees");
+
+    if (_db) {
+      console.log("Successfully connected to MongoDB.");
+    } else {
+      console.log("Failed to connect to MongoDB.");
+    }
+    return callback(_db === undefined ? false : true);
   },
 
   getDb: function () {
